ca1340a06288dd3f8c10cbab480a2b16de85fc64
jeff <tangj1122@gmail.com>
2018-05-06 21:09:08 -0700

    copy in and out

---
+++ copy-out.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# script to copy dotfiles repo from jeff's macbook to system
+
+cp eslintrc.js ~/.eslintrc.js
+cp gitconfig ~/.gitconfig
+cp gitignore_global ~/.gitignore_global
+cp tmux.conf ~/.tmux.conf
+cp -R tmux/ ~/.tmux
+cp -R vim/ ~/.vim/
+cp zshrc ~/.zshrc
