8e2948f0ba0178fbb82aaeeafc1f037e53fff5b3
jeff <tangj1122@gmail.com>
2018-06-30 11:26:16 -0700

    scripts

--- scripts/ubuntu-up.sh
+++ scripts/ubuntu-up.sh
@@ -1,4 +1,5 @@
 # shortcuts: https://help.ubuntu.com/lts/ubuntu-help/shell-keyboard-shortcuts.html.en
+# view ethernet addresses sudo lshw -class network
 
 # # to see all intentionally installed packages
 # (zcat $(ls -tr /var/log/apt/history.log*.gz); cat /var/log/apt/history.log) 2>/dev/null |
@@ -6,7 +7,7 @@
 #   grep -v aptdaemon |
 #   egrep -B1 '^Commandline:'
 
-echo "sudo apt-get install command line apps"
+echo "\nSUDO APT PACKAGES\n"
 sudo apt-get install git -y
 sudo apt-get install vim -y
 sudo apt-get install tmux -y
@@ -19,9 +20,14 @@ sudo apt-get install gnome-tweak-tool -y
 sudo apt-get install xclip -y
 sudo apt-get install bspwm -y
 sudo apt-get install redshift redshift-gtk -y
+echo "[redshift]
+allowed=true
+system=false
+users=" >> /etc/geoclue/geoclue.conf
 sudo apt-get install postgres postgresql-contrib pgadmin3 -y # setup: https://help.ubuntu.com/community/PostgreSQL
 
-echo "curl for yarn"
+echo "\nYARN\n"
+sudo apt-get remove cmdtest
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get install yarn
@@ -30,26 +36,32 @@ npm init -y
 yarn global add nodemon 
 yarn global add eslint eslint-plugin-react
 
+echo "\nTHEFUCK\n"
 sudo apt update
-sudo apt-get install python3-dev python3-pip
-sudo pip3 install thefuck
+sudo apt-get install python3-dev python3-pip -y
+sudo pip3 install thefuck -y
 
-sudo apt-get install openjdk-8-jre
+echo "\nJAVA\n"
+sudo apt-get install openjdk-8-jre -y
 
+echo "\nNGROK\n"
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
 rm -f ngrok-stable-linux-amd64.zip
 
-sudo snap install chromium -y
-sudo snap install postman -y
-sudo snap install spotify -y
-sudo snap install tusk -y
+echo "\nSNAP\n"
+sudo snap install chromium
+sudo snap install postman
+sudo snap install spotify
+sudo snap install tusk
 sudo snap install heroku --classic
 
-echo "clone zpresto"
+echo "\nZPRESTO\n"
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-echo "change shell to zsh (remember to log out / log in to reset)"
-chsh -s /bin/zsh
+echo "\nVUNDLE\n"
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+source ./scripts/nix-in.sh
 
-source ./scripts/ubuntu-up.sh
+echo "\nCHANGE SHELL (RESTART)\n"
+chsh -s /bin/zsh
