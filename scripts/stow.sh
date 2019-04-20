5e465951010ffd0b82acbfd4b3e4776a310ed333
Jeff Tang <tangj1122@gmail.com>
2019-04-19 21:50:44 -0700

    stow some stuff

--- scripts/stow.sh
+++ scripts/stow.sh
@@ -1,5 +1,26 @@
-#!/bin/zsh
+#!/bin/sh
 
-stow .profile -t ~/
-stow zsh
-git@github.com:VundleVim/Vundle.vim.git
+if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+symlink=(
+.profile
+.tmux.conf
+)
+
+for x in ${symlink[*]}; do
+  ln -s $symlink ~/$symlink
+done
+
+
+dir=(
+zsh
+vim
+)
+for x in ${dir[*]}; do
+  if [[ ! -d  ~/.$x ]]; then
+	  mkdir ~/ ~/.$x
+    stow -vR $x -t ~/.$x
+  fi
+done
